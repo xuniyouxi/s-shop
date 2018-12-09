02f561510092a5feddec9847b60e5684a87852b2
lizhenyu0128 <879681805@qq.com>
2018-12-09 23:30:36 +0800

    修改

--- src/main/java/com/vg/controller/user/UserBehaviorController.java
+++ src/main/java/com/vg/controller/user/UserBehaviorController.java
@@ -24,13 +24,20 @@ public class UserBehaviorController {
 	@Autowired
 	UserBehaviorservice userbehaviorservice;
 
-	// 注册 http://localhost:8080/vg/user/register
+	// 注册获取验证码 http://localhost:8080/vg/user/register
 	@RequestMapping(value = "/register")
 	@Authorization(authorization = "open")
 	public BackJSON register() {
 //		System.out.println( UUID.randomUUID().toString().replaceAll("-",""));
 		return null;
 
+	}
+
+	// 注册设置密码 http://localhost:8080/vg/user/SetPassword
+	@PostMapping({ "/register" })
+	@Authorization(authorization = "open")
+	public BackJSON register(@RequestBody User user) throws Exception {
+		return userbehaviorservice.SetPassword(user);
 	}
 
 	// 登陆 http://localhost:8080/vg/user/login
@@ -47,15 +54,8 @@ public class UserBehaviorController {
 	public BackJSON getStatement(@PathVariable int bis_id) {
 		return userbehaviorservice.getStatementByFun(bis_id);
 	}
-	
-	//修改密码通过个人信息	http://localhost:8080/vg/user/SetPassword
-	@PostMapping({"/SetPassword"})
-	@Authorization(authorization = "open")
-	public BackJSON SetPassword(@RequestBody User user) throws Exception {
-		return userbehaviorservice.SetPassword(user);
-	}
-	
-	//填写激活码
+
+	// 填写激活码
 //	@PostMapping({"/SetPassword"})
 
 }
